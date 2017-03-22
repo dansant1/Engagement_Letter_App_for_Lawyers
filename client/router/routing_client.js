e3a49c6a582ef118e7762f85d854d784e4e167d2
Daniel Delgadillo <danieldelgadilloh@gmail.com>
2017-03-21 19:53:05 -0500

    first screen

---
+++ client/router/routing_client.js
@@ -0,0 +1,84 @@
+FlowRouter.route('/home', {
+  name: 'Home',
+  action() {
+    BlazeLayout.render('Backoffice', { page: 'home' })
+  }
+})
+
+FlowRouter.route('/auth', {
+  name: 'Auth',
+  action() {
+    BlazeLayout.render('Auth', { step: 'Auth_Step_1' })
+  }
+})
+
+FlowRouter.route('/', {
+  name: 'Auth',
+  action() {
+    BlazeLayout.render('Auth', { step: 'Auth_Step_1' })
+  }
+})
+
+FlowRouter.route('/auth_2', {
+  name: 'Auth',
+  action() {
+    BlazeLayout.render('Auth', { step: 'Auth_Step_2' })
+  }
+})
+
+FlowRouter.route('/list_letters', {
+  name: 'List of Letters',
+  action() {
+    BlazeLayout.render('List_Letters', { page: 'letters'})
+  }
+})
+
+FlowRouter.route('/setup', {
+  name: 'Setup.',
+  action() {
+    BlazeLayout.render('Backoffice', { page: 'Setup' })
+  }
+})
+
+FlowRouter.route('/payments', {
+  name: 'List of Payments',
+  action() {
+    BlazeLayout.render('Payments', { page: 'list_of_payments'})
+  }
+})
+
+FlowRouter.route('/new_letter', {
+  name: 'New Letter Step 1',
+  action() {
+    BlazeLayout.render('New_Letter', { step: 'Write_Letter'})
+  }
+})
+
+FlowRouter.route('/new_letter/step_2', {
+  name: 'New Letter Step 2',
+  action() {
+    BlazeLayout.render('New_Letter', { step: 'Write_Exclusion'})
+  }
+})
+
+FlowRouter.route('/new_letter/step_3', {
+  name: 'New Letter Step 3',
+  action() {
+    BlazeLayout.render('New_Letter', { step: 'Lawyer_Rates'})
+  }
+})
+
+// FlowRouter.route('/view_letter/:letterId', {
+//   name: 'View Letter',
+//   action() {
+//     BlazeLayout.render('View_Letter', { letter: 'Letter_Layout'})
+//   }
+// })
+
+
+FlowRouter.route('/view_letter', {
+  name: 'View Letter',
+  action() {
+    BlazeLayout.render('Letter_Layout', { letter: 'View_Letter'})
+  }
+})
