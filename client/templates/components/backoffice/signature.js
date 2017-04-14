6826caddd1c5a4fbb278986e67cc1a85e802117f
Daniel Delgadillo Huaman <danieldelgadilloh@gmail.com>
2017-04-14 17:58:07 -0500

    draw signature

--- client/templates/components/backoffice/signature.js
+++ client/templates/components/backoffice/signature.js
@@ -1,51 +1,69 @@
-function createCanvas(parent, width, height) {
-        var canvas = {};
-        canvas.node = document.createElement('canvas');
-        canvas.node.className += "draw" 
-        canvas.context = canvas.node.getContext('2d');
-        canvas.node.width = width || 100;
-        canvas.node.height = height || 100;
-        parent.appendChild(canvas.node);
-        return canvas;
-    }
 
-    function init(container, width, height, fillColor) {
-        var canvas = createCanvas(container, 900,400);
-        var ctx = canvas.context;
-        // define a custom fillCircle method
-        ctx.fillCircle = function(x, y, radius, fillColor) {
-            this.fillStyle = fillColor;
-            this.beginPath();
-            this.moveTo(x, y);
-            this.arc(x, y, radius, 0, Math.PI * 2, false);
-            this.fill();
-        };
-        ctx.clearTo = function(fillColor) {
-            ctx.fillStyle = fillColor;
-            ctx.fillRect(0, 0, width, height);
-        };
-        ctx.clearTo(fillColor || "#ddd");
-
-        // bind mouse events
-        canvas.node.onmousemove = function(e) {
-            if (!canvas.isDrawing) {
-               return;
-            }
-            var x = e.pageX - this.offsetLeft;
-            var y = e.pageY - this.offsetTop;
-            var radius = 2// or whatever
-            var fillColor = '#000000';
-            ctx.fillCircle(x, y, radius, fillColor);
-        };
-        canvas.node.onmousedown = function(e) {
-            canvas.isDrawing = true;
-        };
-        canvas.node.onmouseup = function(e) {
-            canvas.isDrawing = false;
-        };
-    }
 
 Template.DrawSignature.onRendered(() => {
-    var container = document.getElementById('canvas');
-    init(container, 600, 400, '#ddd');
+    var canvasDiv = document.getElementById('canvasDiv');
+    canvas = document.createElement('canvas');
+    canvas.setAttribute('width', 600);
+    canvas.setAttribute('height', 400);
+    canvas.setAttribute('id', 'canvas');
+    canvasDiv.appendChild(canvas);
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
+
+    function redraw(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      
+      context.strokeStyle = "#000000";
+      context.lineJoin = "round";
+      context.lineWidth = 2;
+            
+      for(var i=0; i < clickX.length; i++) {        
+        context.beginPath();
+        if(clickDrag[i] && i){
+          context.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.stroke();
+      }
+    }
+    
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+    
+    if (typeof G_vmlCanvasManager != 'undefined') canvas = G_vmlCanvasManager.initElement(canvas)
+
+    context = canvas.getContext("2d");
+
+    $('#canvas').mousedown(function(e){
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+            
+      paint = true;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      redraw();
+    });
+
+    $('#canvas').mousemove(function(e){
+        if(paint){
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        } 
+    });
+
+    $('#canvas').mouseup(function(e){
+        paint = false;
+    });
+
+    $('#canvas').mouseleave(function(e){
+        paint = false;
+    });
 })
