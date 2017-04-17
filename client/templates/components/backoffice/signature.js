79f2d439bd84469a279f2c75b8b2a76504844dd9
Daniel Delgadillo <danieldelgadilloh@gmail.com>
2017-04-16 22:44:29 -0500

    draw and write signature req added

--- client/templates/components/backoffice/signature.js
+++ client/templates/components/backoffice/signature.js
@@ -1,39 +1,44 @@
 
 
 Template.DrawSignature.onRendered(() => {
-    var canvasDiv = document.getElementById('canvasDiv');
+    let template = Template.instance()
+    template.signature = new ReactiveVar("")
+    template.font = new ReactiveVar("Great Vibes")
+    template.canvas =  document.getElementById("signature__text");
+    template.ctx = template.canvas.getContext("2d");
+    let canvasDiv = document.getElementById('canvasDiv');
     canvas = document.createElement('canvas');
-    canvas.setAttribute('width', 600);
+    canvas.setAttribute('width', 500);
     canvas.setAttribute('height', 400);
     canvas.setAttribute('id', 'canvas');
     canvasDiv.appendChild(canvas);
 
-    var clickX = new Array();
-    var clickY = new Array();
-    var clickDrag = new Array();
-    var paint;
+    let clickX = new Array();
+    let clickY = new Array();
+    let clickDrag = new Array();
+    let paint;
 
-    function redraw(){
+    let redraw = () => {
       context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
       
       context.strokeStyle = "#000000";
       context.lineJoin = "round";
       context.lineWidth = 2;
             
-      for(var i=0; i < clickX.length; i++) {        
+      for (let i = 0; i < clickX.length; i++) {        
         context.beginPath();
-        if(clickDrag[i] && i){
+        if (clickDrag[i] && i){
           context.moveTo(clickX[i-1], clickY[i-1]);
-         }else{
-           context.moveTo(clickX[i]-1, clickY[i]);
-         }
-         context.lineTo(clickX[i], clickY[i]);
-         context.closePath();
-         context.stroke();
+        } else{
+          context.moveTo(clickX[i]-1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.stroke();
       }
     }
     
-    function addClick(x, y, dragging) {
+    let addClick = (x, y, dragging) => {
         clickX.push(x);
         clickY.push(y);
         clickDrag.push(dragging);
@@ -66,4 +71,48 @@ Template.DrawSignature.onRendered(() => {
     $('#canvas').mouseleave(function(e){
         paint = false;
     });
+
+    template.autorun( () => {
+      template.ctx.font = `30px ${template.font.get()}`
+      template.ctx.fillText(template.signature.get(), 10, 50);
+    })
+    
+    
+})
+
+
+Template.DrawSignature.events({
+  'keyup [name="signature"]'(e, t) {
+    
+    t.ctx.clearRect(0, 0, t.canvas.width, t.canvas.height);  
+    t.signature.set(e.target.value)
+
+  },
+  'click .sf'(e, t) {
+    let font = $( e.target ).data( "font" )
+
+    $( '.sd' ).prop('disabled', false)      
+
+    $( e.target ).prop('disabled', true)
+    switch (font) {
+      case "01": 
+        $('.drop span').text('Type 01')
+        t.ctx.clearRect(0, 0, t.canvas.width, t.canvas.height); 
+        t.font.set('Great Vibes') 
+        break;
+      
+      case "02": 
+        $('.drop span').text('Type 02')
+        t.ctx.clearRect(0, 0, t.canvas.width, t.canvas.height);
+        t.font.set('Playball') 
+        break;
+      
+      case "03": 
+        $('.drop span').text('Type 03')
+        t.ctx.clearRect(0, 0, t.canvas.width, t.canvas.height);
+        t.font.set('Marck Script') 
+        break;
+      
+    }
+  }
 })
--- server/s3/config.js
+++ server/s3/config.js
@@ -1,17 +1,17 @@
 Slingshot.fileRestrictions( "Upload", {
   allowedFileTypes: [ "image/png", "image/jpeg"],
   maxSize: 1 * 1024 * 1024
-});
+})
 
 Slingshot.createDirective( "Upload", Slingshot.S3Storage, {
   bucket: "engagement-letter-app",
   acl: "public-read",
-  authorize: function () {
+  authorize() {
     let userFileCount = Signatures.find( { "userId": this.userId } ).count();
     return userFileCount < 3 ? true : false;
   },
-  key: function ( file ) {
+  key( file ) {
     var user = Meteor.users.findOne( this.userId );
     return user.emails[0].address + "/" + file.name;
   }
-});
+})
